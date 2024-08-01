3365e0a60bc56d1fe111a3836f518cd5ea171b99
romiyadangol <dangolromiya@gmail.com>
2024-08-01 10:38:05 +0545

    Added add and get methods

--- password_manager.rb
+++ password_manager.rb
@@ -3,6 +3,56 @@ require 'securerandom'
 
 
 class PasswordManager
+    def initialize()#initialize
+        @passwords = {} #hash
+    end
+
+    #add password
+    def add_password(service, username, password)
+        @passwords[service] = {
+            username = username,
+            password = encrypt_password(password)
+        }
+        save_passwords
+    end
+
+    #get password
+    def get_password(service)
+        if @passwords.key?(service)
+            password = @passwords[service][:password]
+            decrypt_password(password)
+        else
+            "Service Not Found"
+        end
+    end
+
+    #get username
+    def username(service)
+        if @passwords.key?(service)
+            @passwords[service][:username]
+
+    end
+
+    private
+    #encrypts message
+    def encrypt_password(password)
+        BCrypt::Password.create(password)
+    end
+
+    #decrypts message
+    def decrypt_password(encrypt_password)
+        BCrypt::Password.new(encrypt_password)
+    end
+
+    def load_password
+
+    end
+
+    def save_passwords
+
+    end
+
+
 
 end
 
@@ -27,6 +77,24 @@ loop do
     choice = gets.chomp.to_i
 
     case choice
+    when 1
+        puts "Enter Service"
+        service = gets.chomp
+
+        puts "Enter Username"
+        username = gets.chomp
+
+        puts "Enter Password"
+        password = gets.chomp
+
+        password_manager.add_password(service, username, password)
+
+    when 2
+        puts "Enter Service"
+        service = gets.chomp
+
+        puts "Username: #{password_manager.get_username(service)}"
+        puts "Password: #{password_manager.get_password(service)}"
 
     end
 end
